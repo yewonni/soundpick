111bbea0d4f88ad9ecb43f9c97ef81436b7b78d6
ddhsl <dnjs9199@naver.com>
2025-04-04 22:39:08 +0900

    [add] MyFriends 컴포넌트 추가

---
+++ src/pages/myFriends/MyFriends.tsx
@@ -0,0 +1,63 @@
+import prevIcon from "../../images/chevron-left.svg";
+import { useNavigate } from "react-router-dom";
+import sample from "../../images/sample.png";
+import chatIcon from "../../images/chat-icon.svg";
+
+const mockData = [
+  {
+    imageUrl: sample,
+    username: "요를레이후",
+  },
+  {
+    imageUrl: sample,
+    username: "굳은 총잡이",
+  },
+  {
+    imageUrl: sample,
+    username: "화가난 연두부",
+  },
+  {
+    imageUrl: sample,
+    username: "강아지체고야",
+  },
+];
+
+export default function MyFriends() {
+  const navigate = useNavigate();
+  return (
+    <>
+      <header className="p-4 bg-bg-sub flex justify-center items-center relative">
+        <img
+          src={prevIcon}
+          alt="이전으로 가기"
+          className="cursor-pointer absolute left-4"
+          onClick={() => navigate(-1)}
+        />
+        <h1 className="font-bold text-lg">나의 음악 친구</h1>
+      </header>
+      <main className="w-full min-h-screen bg-white p-4">
+        <p className="font-bold text-primary py-1  border-b border-b-primary ">
+          총 4명
+        </p>
+        {mockData.map((data, index) => (
+          <article
+            key={index}
+            className="flex justify-between items-center py-3 mt-2"
+          >
+            <div className="flex gap-4 items-center">
+              <img
+                src={data.imageUrl}
+                alt=""
+                className="w-[50px] h-[50px] rounded-[100px] cursor-pointer"
+              />
+              <p className="font-bold text-sm cursor-pointer">
+                {data.username}
+              </p>
+            </div>
+            <img src={chatIcon} alt="" className="cursor-pointer" />
+          </article>
+        ))}
+      </main>
+    </>
+  );
+}
