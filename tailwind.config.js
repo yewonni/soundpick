db05f1db1afe7368cc86da8f6078b6fcbdcf8a3e
ddhsl <dnjs9199@naver.com>
2025-04-04 22:27:55 +0900

    [edit] tailwind.config 메인 컬러 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,22 @@ module.exports = {
   ],
 
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#7D3C98",
+        secondary: "#8e8d8d",
+        purple: {
+          100: "#cfcfcf",
+          200: "#7a7a7a",
+          400: "#b1b6f8",
+          500: "#8887f2",
+          600: "#5740c2",
+        },
+        bg: {
+          sub: "#F6FBF0",
+        },
+      },
+    },
   },
   plugins: [],
 };
