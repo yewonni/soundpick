9baae5e563fa8d5037fe88cc56b6c027167c0617
ddhsl <dnjs9199@naver.com>
2025-04-04 22:40:49 +0900

    [add] MyAllTimeHits 컴포넌트 추가

---
+++ src/pages/myPage/component/MyAllTimeHits.tsx
@@ -0,0 +1,129 @@
+import prevIcon from "../../../images/chevron-left.svg";
+import FinishButton from "../../../components/FinishButton";
+import switchIcon from "../../../images/switch.svg";
+import RegisterButton from "../../../components/RegisterButton";
+import sample from "../../../images/sample.png";
+import { useNavigate } from "react-router-dom";
+
+const mockData = [
+  {
+    imageUrl: sample,
+    title: "Bohemian Rhapsody",
+    subTitle: "Queen",
+  },
+  {
+    imageUrl: sample,
+    title: "Shape of You",
+    subTitle: "Ed Sheeran",
+  },
+  {
+    imageUrl: sample,
+    title: "Billie Jean",
+    subTitle: "Michael Jackson",
+  },
+  {
+    imageUrl: sample,
+    title: "Someone Like You",
+    subTitle: "Adele",
+  },
+  {
+    imageUrl: sample,
+    title: "Rolling in the Deep",
+    subTitle: "Adele",
+  },
+  {
+    imageUrl: sample,
+    title: "Hotel California",
+    subTitle: "Eagles",
+  },
+  {
+    imageUrl: sample,
+    title: "Smells Like Teen Spirit",
+    subTitle: "Nirvana",
+  },
+  {
+    imageUrl: sample,
+    title: "Uptown Funk",
+    subTitle: "Mark Ronson ft. Bruno Mars",
+  },
+  {
+    imageUrl: sample,
+    title: "Blinding Lights",
+    subTitle: "The Weeknd",
+  },
+  {
+    imageUrl: sample,
+    title: "Thinking Out Loud",
+    subTitle: "Ed Sheeran",
+  },
+];
+
+export default function MyAllTimeHits() {
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <header className="p-4 bg-bg-sub flex justify-between">
+        <img
+          src={prevIcon}
+          alt="이전으로 가기"
+          className="cursor-pointer"
+          onClick={() => navigate(-1)}
+        />
+        <h1 className="font-bold text-lg">My All-Time Hits</h1>
+        <FinishButton />
+      </header>
+      <main className="p-4">
+        <div className="flex flex-col text-sm font-semibold text-gray-600 mb-4">
+          <p>
+            <span className="text-primary font-bold">에옹님</span>이
+            <span className="text-primary font-bold underline">
+              {" "}
+              가장 사랑하는 곡
+            </span>
+            들을 모아둔 공간이에요.
+          </p>
+          <p>
+            언제든 자유롭게 업데이트하세요!
+            <span className="text-xs text-secondary"> (최대 10곡)</span>
+          </p>
+        </div>
+        <section>
+          <h2 className="sr-only">나의 All time hits 목록</h2>
+          <div className="bg-white w-full rounded-lg shadow-lg p-4 pb-6">
+            {mockData.map((song, index) => (
+              <article
+                key={index}
+                className="border-b border-b-gray-200 flex items-center gap-5 py-3 relative"
+              >
+                <p className="text-secondary pl-3">{index + 1}</p>
+                <div className="flex gap-3 items-center">
+                  <img
+                    src={song.imageUrl}
+                    alt={song.title}
+                    className="w-[50px] h-[50px] rounded-sm"
+                  />
+                  <div className="flex flex-col gap-1">
+                    <h3 className="font-bold text-sm">{song.title}</h3>
+                    <p className="text-xs text-secondary">{song.subTitle}</p>
+                  </div>
+                </div>
+                <img
+                  src={switchIcon}
+                  alt="변경하기"
+                  className="absolute right-2 cursor-pointer"
+                  onClick={() => navigate("/search-artist")}
+                />
+              </article>
+            ))}
+          </div>
+        </section>
+        <div className="mt-4 mb-7">
+          <RegisterButton onClick={() => navigate("/search-artist")}>
+            곡 추가하기
+          </RegisterButton>
+        </div>
+      </main>
+    </>
+  );
+}
