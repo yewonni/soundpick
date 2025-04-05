1fcff6163f82f01ba6f63ad11ce23443874ddec2
ddhsl <dnjs9199@naver.com>
2025-04-06 01:40:37 +0900

    [add] ArtistSearch 컴포넌트 추가

---
+++ src/pages/ musicAnalysis/component/ArtistSearch.tsx
@@ -0,0 +1,110 @@
+import { useNavigate, useLocation } from "react-router-dom";
+import { useState } from "react";
+import backIcon from "../../../images/chevron-left.svg";
+import FinishButton from "../../../components/FinishButton";
+import SearchBar from "../../../components/SearchBar";
+import sample from "../../../images/sample.png";
+import plus from "../../../images/plus-icon.svg";
+import check from "../../../images/check-on.svg";
+
+const topArtistList: string[] = [
+  "The Weekend",
+  "Ariana Grande",
+  "Billie Eilish",
+  "Ed Sheeran",
+  "Taylor Swift",
+  "Charlie Puth",
+  "Drake",
+  "Kendrick Lamar",
+  // "LANY",
+  // "Anne-Marie",
+];
+
+export default function ArtistSearch() {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const [selected, setSelected] = useState<boolean[]>(
+    Array(topArtistList.length).fill(false)
+  );
+
+  const toggleSelect = (index: number) => {
+    setSelected((prev) => {
+      const newState = [...prev];
+      newState[index] = !newState[index];
+      return newState;
+    });
+  };
+
+  const handleBack = () => {
+    if (location.state?.fromStep2) {
+      navigate("/music-analysis", { state: { currentStep: 2 } });
+    } else {
+      navigate(-1);
+    }
+  };
+
+  return (
+    <>
+      <header className="bg-bg-sub px-4 py-5 md:px-[10%] md:py-6 flex justify-between items-center">
+        {/* 왼쪽: 뒤로가기 */}
+        <div className="flex items-center gap-3 md:gap-8">
+          <img
+            src={backIcon}
+            alt="이전으로 돌아가기"
+            className="cursor-pointer md:w-6  md:hidden"
+            onClick={handleBack}
+          />
+          <p className="text-lg md:text-2xl font-bold">아티스트 선택</p>
+        </div>
+
+        {/* 오른쪽: 진행 상황 버튼 */}
+        <div className="flex gap-2 items-center text-primary font-bold md:bg-white md:px-6 md:py-1 md:rounded-lg md:border-2 md:border-purple-700 md:cursor-pointer md:hover:bg-gray-200 md:active:bg-purple-400">
+          <p className="text-[20px] md:text-[24px] ">1</p>
+          <FinishButton onClick={handleBack} />
+        </div>
+      </header>
+
+      <main className=" w-full min-h-screen md:flex md:px-[10%] md:py-6">
+        {/* 왼쪽 영역 */}
+        <section className="w-full md:w-2/3 md:pr-11">
+          <div className="p-4 md:p-0">
+            <SearchBar placeholder="음악, 아티스트 검색하기" />
+          </div>
+
+          <div className="mt-4 space-y-2">
+            {topArtistList.map((artist, index) => (
+              <article
+                key={index}
+                className="flex items-center justify-between px-4 py-2 md:px-0  rounded cursor-pointer"
+              >
+                <div className="flex gap-4 items-center">
+                  <img
+                    src={sample}
+                    alt=""
+                    className="w-[50px] h-[50px] rounded-sm"
+                  />
+                  <h2 className="font-bold text-sm md:text-base">{artist}</h2>
+                </div>
+                <button onClick={() => toggleSelect(index)}>
+                  <img src={selected[index] ? check : plus} alt="추가하기" />
+                </button>
+              </article>
+            ))}
+          </div>
+        </section>
+
+        {/* 오른쪽 영역: 데스크탑 전용 */}
+        <aside className="hidden md:block md:w-1/3  bg-white opacity-40 rounded-lg shadow-lg p-5 h-[500px]">
+          <h3 className="text-lg font-semibold mb-3">선택한 아티스트</h3>
+          <ul className="text-sm text-gray-700 space-y-1">
+            {/* {topArtistList.map(
+              (artist, index) =>
+                selected[index] && <li key={index}>{artist}</li>
+            )} */}
+            <li className="text-center">아티스트를 선택해 주세요.</li>
+          </ul>
+        </aside>
+      </main>
+    </>
+  );
+}
