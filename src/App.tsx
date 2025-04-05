0abff774ced26fcfb2550cf6f706ac9edd69870f
ddhsl <dnjs9199@naver.com>
2025-04-06 01:23:00 +0900

    [add] 라우터 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,63 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./Home";
+import Search from "./pages/search/Search";
+import SearchResult from "./pages/searchResult/SearchResult";
+import PlaylistDetails from "./pages/playlistDetails/PlaylistDetails";
+import Login from "./pages/login/Login";
+import Join from "./pages/join/Join";
+import JoinSuccess from "./pages/join/JoinSuccess";
+import MusicAnalysis from "./pages/ musicAnalysis/ MusicSurvey";
+import ArtistSearch from "./pages/ musicAnalysis/component/ArtistSearch";
+import Recommendation from "./pages/ musicAnalysis/component/Recommendation";
+import Review from "./pages/review/Review";
+import MyPage from "./pages/myPage/MyPage";
+import MyAllTimeHits from "./pages/myPage/component/MyAllTimeHits";
+import MyPlaylist from "./pages/myPage/component/MyPlaylist";
+import MyPlaylistDetails from "./pages/myPlaylistDetails/MyPlaylistDetails";
+import LikedPlaylists from "./pages/myPage/component/LikedPlaylists";
+import LikedPlaylistDetails from "./pages/likedPlaylistDetails/LikedPlaylistDetails";
+import ErrorPage from "./pages/404error/ErrorPage";
+import RegisterPlaylist from "./pages/registerPlaylist/RegisterPlaylist";
+import MyReview from "./pages/myReview/MyReview";
+import MyFriends from "./pages/myFriends/MyFriends";
+import EditProfile from "./pages/editProfile/EditProfile";
 
-// React.FC 타입을 사용하여 컴포넌트가 올바른 JSX 요소를 반환하도록 지정
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p className="text-3xl">
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/search" element={<Search />}></Route>
+        <Route path="/search-result" element={<SearchResult />}></Route>
+        <Route path="/playlist-details" element={<PlaylistDetails />}></Route>
+        <Route path="/review" element={<Review />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/join" element={<Join />}></Route>
+        <Route path="/join/success" element={<JoinSuccess />}></Route>
+        <Route path="/music-analysis" element={<MusicAnalysis />}></Route>
+        <Route path="/search-artist" element={<ArtistSearch />}></Route>
+        <Route path="/recommendation" element={<Recommendation />}></Route>
+        <Route path="/mypage" element={<MyPage />}></Route>
+        <Route path="/my-hits" element={<MyAllTimeHits />}></Route>
+        <Route path="/my-playlist" element={<MyPlaylist />}></Route>
+        <Route
+          path="/my-playlist-details"
+          element={<MyPlaylistDetails />}
+        ></Route>
+        <Route path="/register-playlist" element={<RegisterPlaylist />}></Route>
+        <Route path="/liked-playlist" element={<LikedPlaylists />}></Route>
+        <Route
+          path="/liked-playlist-details"
+          element={<LikedPlaylistDetails />}
+        ></Route>
+        <Route path="/my-review" element={<MyReview />}></Route>
+        <Route path="/my-friends" element={<MyFriends />}></Route>
+        <Route path="/edit-profile" element={<EditProfile />}></Route>
+        <Route path="/*" element={<ErrorPage />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 };
 
