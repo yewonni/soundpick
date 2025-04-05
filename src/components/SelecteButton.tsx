b036ae01e56cb151a58b4791401b3bba249cc9b2
ddhsl <dnjs9199@naver.com>
2025-04-06 01:37:51 +0900

    [edit] SelectButton : 디자인 및 컬러 변경

--- src/components/SelecteButton.tsx
+++ src/components/SelecteButton.tsx
@@ -14,8 +14,10 @@ export default function SelectButton({ children }: SelectButtonProps) {
   return (
     <button
       onClick={handleClick}
-      className={`w-full flex justify-center border border-purple-600 p-2 md:p-3 text-md text-black font-bold rounded-[12px] ${
-        selected ? "bg-[#EAE3FE] " : "hover:bg-gray-100"
+      className={`w-full flex justify-center border border-primary p-2 md:p-3 text-md font-bold rounded-[12px] transition-colors duration-200 ${
+        selected
+          ? "bg-white/60 text-purple-800"
+          : "bg-transparent text-[#333] hover:bg-white/20"
       }`}
     >
       {children}
