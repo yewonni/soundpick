baf97270543d4444542bc2d39d1bf808992ca1dc
ddhsl <dnjs9199@naver.com>
2025-04-06 01:26:36 +0900

    [edit] SearchResult 컴포넌트 반응형 추가 및 컬러 수정

--- src/pages/searchResult/SearchResult.tsx
+++ src/pages/searchResult/SearchResult.tsx
@@ -5,6 +5,9 @@ import SearchBar from "../../components/SearchBar";
 import sample from "../../images/sample.png";
 import sideMenu from "../../images/side-menu.svg";
 import PlaylistModal from "./component/PlaylistModal";
+import Header from "../../components/Header";
+import Checkbox from "../../components/Checkbox";
+import BackgroundWrapper from "../../components/BackgroundWrapper";
 
 type resultListItem = {
   imageUrl: string;
@@ -20,7 +23,7 @@ const resultList: resultListItem[] = [
   { imageUrl: sample, title: "22", subTitle: "Taylor Swift" },
   {
     imageUrl: sample,
-    title: "I don’t think that I like her",
+    title: "I don't think that I like her",
     subTitle: "Charlie Puth",
   },
 ];
@@ -44,94 +47,185 @@ export default function SearchResult() {
 
   return (
     <>
-      <header className="flex justify-center items-center px-4 py-2 h-[70px] bg-bg-sub">
-        <h1>
-          <img
-            src={logo}
-            alt="로고"
-            className="cursor-pointer"
-            onClick={() => navigate("/")}
-          />
-        </h1>
-      </header>
-      <div className="p-4">
-        <SearchBar />
-        <div className="flex items-center gap-2 text-sm text-secondary font-semibold pl-2 mt-4">
-          <p
-            className={`cursor-pointer ${
-              isResultType === "song" ? "text-primary font-bold" : ""
-            }`}
-            onClick={() => handleResultType("song")}
-          >
-            곡
-          </p>
-          <span className="w-px h-3 bg-secondary"></span>
-          <p
-            className={`cursor-pointer ${
-              isResultType === "playlist" ? "text-primary font-bold" : ""
-            }`}
-            onClick={() => handleResultType("playlist")}
-          >
-            플레이리스트
-          </p>
+      <BackgroundWrapper>
+        <div className="hidden md:block">
+          <Header />
         </div>
-      </div>
-
-      <main className="bg-white w-full min-h-screen p-4 pt-5">
-        {isResultType === "song" &&
-          resultList.map((item, index) => (
-            <article
-              key={index}
-              className="flex justify-between items-center mb-4 cursor-pointer"
+        <header className="flex justify-center items-center px-4 py-2 h-[70px] bg-bg-sub md:hidden">
+          <h1>
+            <img
+              src={logo}
+              alt="로고"
+              className="cursor-pointer md:w-[250px] md:h-[150px]"
+              onClick={() => navigate("/")}
+            />
+          </h1>
+        </header>
+        <div className="px-4 pt-4 pb-0 md:px-[10.2%] md:py-0 bg-[#3E3A6D]">
+          <div className="md:hidden">
+            <SearchBar />
+          </div>
+          <div className="flex items-center gap-4 text-sm md:text-base text-secondary font-semibold pl-2 mt-4 md:mt-0 md:pt-5">
+            <p
+              className={`cursor-pointer pb-1 ${
+                isResultType === "song"
+                  ? "text-text-base font-bold border-b-2 border-text-base"
+                  : ""
+              }`}
+              onClick={() => handleResultType("song")}
             >
-              <div className="flex gap-4 items-center">
-                <img
-                  src={item.imageUrl}
-                  alt={item.title}
-                  className="w-[50px] h-[50px] rounded-sm"
-                />
-                <div className="flex flex-col gap-1">
-                  <h2 className="font-bold text-sm">{item.title}</h2>
-                  {item.subTitle && (
-                    <p className="text-secondary text-xs">{item.subTitle}</p>
-                  )}
-                </div>
-              </div>
-              <img
-                src={sideMenu}
-                alt="메뉴열기"
-                className="cursor-pointer"
-                onClick={handlePlaylistModalOpen}
-              />
-            </article>
-          ))}
-        {isResultType === "playlist" &&
-          resultList.map((item, index) => (
-            <article
-              key={index}
-              className=" mb-4 cursor-pointer"
-              onClick={() => navigate("/playlist-details")}
+              곡
+            </p>
+            <p
+              className={`cursor-pointer pb-1 ${
+                isResultType === "playlist"
+                  ? "text-text-base font-bold border-b-2  border-text-base"
+                  : ""
+              }`}
+              onClick={() => handleResultType("playlist")}
             >
-              <div className="flex gap-4 items-start">
-                <img
-                  src={item.imageUrl}
-                  alt={item.title}
-                  className="w-[100px] h-[100px] rounded-sm"
-                />
-                <div className="flex flex-col gap-3 pt-3">
-                  <h2 className="font-bold text-md">{item.title}</h2>
-                  {item.subTitle && (
-                    <p className="text-secondary text-sm">{item.subTitle}</p>
-                  )}
-                </div>
-              </div>
-            </article>
-          ))}
-      </main>
+              플레이리스트
+            </p>
+          </div>
+        </div>
 
-      {isPlaylistModalOpen && (
-        <PlaylistModal onClose={handlePlaylistModalClose} />
-      )}
+        <main className="w-full min-h-screen p-4 pt-5 md:px-[10.7%] bg-[#3E3A6D] ">
+          {isResultType === "song" && (
+            <div className="flex gap-2  w-full border-b-2 border-b-gray-300 pl-3  pb-3 ">
+              <Checkbox type="circle" />
+              <button className="ml-3 border border-text-subtle px-3 p-1 text-xs text-text-subtle font-semibold rounded-sm hover:bg-[#bfcBFF26] active:bg-[#bfcBFF4D] hover:border-[#BFCBFF] active:border-[#aab8f5] transition-colors duration-200">
+                듣기
+              </button>
+              <button className="border border-text px-3 p-1 text-xs text-text-subtle font-semibold rounded-sm hover:bg-[#bfcBFF26] active:bg-[#bfcBFF4D] hover:border-[#BFCBFF] active:border-[#aab8f5] transition-colors duration-200">
+                내 플리에 담기
+              </button>
+            </div>
+          )}
+
+          {/* 데스크탑 전용 테이블 헤더 */}
+          {isResultType === "song" && (
+            <div className="hidden md:grid md:grid-cols-[40px_80px_1fr_1fr_40px] text-sm font-bold text-text-base py-3 border-b border-gray-300 mb-4">
+              <div className="flex justify-center items-center"></div>
+              <div className="flex items-center justify-center"></div>
+              <div className="flex items-center px-2">곡</div>
+              <div className="flex items-center px-2">아티스트</div>
+              <div className="flex justify-center items-center">기타</div>
+            </div>
+          )}
+
+          {/* 곡 리스트 */}
+          {isResultType === "song" &&
+            resultList.map((item, index) => (
+              <article key={index} className="mb-4 cursor-pointer">
+                {/* 데스크탑 버전 */}
+                <div className="hidden md:grid md:grid-cols-[40px_80px_1fr_1fr_40px] md:items-center py-2">
+                  <div className="flex justify-center items-center">
+                    <Checkbox type="circle" />
+                  </div>
+                  <div className="flex items-center justify-center">
+                    <img
+                      src={item.imageUrl}
+                      alt={item.title}
+                      className="w-[60px] h-[60px] rounded-sm"
+                    />
+                  </div>
+                  <div className="flex items-center px-2 font-bold text-sm hover:underline text-text-base">
+                    {item.title}
+                  </div>
+                  <div className="flex items-center px-2 text-secondary text-sm">
+                    {item.subTitle}
+                  </div>
+                  <div className="flex justify-center items-center">
+                    <img
+                      src={sideMenu}
+                      alt="메뉴열기"
+                      className="cursor-pointer"
+                      onClick={handlePlaylistModalOpen}
+                    />
+                  </div>
+                </div>
+
+                {/* 모바일 버전 */}
+                <div className="flex md:hidden justify-between items-center w-full px-3 mt-3">
+                  <div className="flex items-center gap-5">
+                    <Checkbox type="circle" />
+                    <img
+                      src={item.imageUrl}
+                      alt={item.title}
+                      className="w-[50px] h-[50px] rounded-sm"
+                    />
+                    <div className="flex flex-col gap-1">
+                      <h2 className="font-bold text-sm">{item.title}</h2>
+                      {item.subTitle && (
+                        <p className="text-secondary text-xs">
+                          {item.subTitle}
+                        </p>
+                      )}
+                    </div>
+                  </div>
+                  <img
+                    src={sideMenu}
+                    alt="메뉴열기"
+                    className="cursor-pointer ml-2"
+                    onClick={handlePlaylistModalOpen}
+                  />
+                </div>
+              </article>
+            ))}
+          {/*데스크탑용 플리*/}
+          {isResultType === "playlist" && (
+            <div className="hidden md:grid md:grid-cols-2 lg:grid-cols-3 gap-6 mt-3">
+              {resultList.map((item, index) => (
+                <div
+                  key={index}
+                  onClick={() => navigate("/playlist-details")}
+                  className="cursor-pointer flex flex-col bg-white shadow-sm hover:shadow-md transition-transform duration-300 transform hover:scale-105 rounded-lg overflow-hidden"
+                >
+                  <img
+                    src={item.imageUrl}
+                    alt={item.title}
+                    className="w-full h-[180px] object-cover"
+                  />
+                  <div className="p-4">
+                    <h2 className="font-bold text-lg truncate">{item.title}</h2>
+                    <p className="text-secondary text-sm mt-1">
+                      {item.subTitle}
+                    </p>
+                  </div>
+                </div>
+              ))}
+            </div>
+          )}
+
+          {/*모바일용 플리*/}
+          {isResultType === "playlist" &&
+            resultList.map((item, index) => (
+              <article
+                key={index}
+                className="mb-4 cursor-pointer md:hidden px-2"
+                onClick={() => navigate("/playlist-details")}
+              >
+                <div className="flex gap-4 items-start">
+                  <img
+                    src={item.imageUrl}
+                    alt={item.title}
+                    className="w-[100px] h-[100px] rounded-sm"
+                  />
+                  <div className="flex flex-col gap-3 pt-3">
+                    <h2 className="font-bold text-md">{item.title}</h2>
+                    {item.subTitle && (
+                      <p className="text-secondary text-sm">{item.subTitle}</p>
+                    )}
+                  </div>
+                </div>
+              </article>
+            ))}
+        </main>
+
+        {isPlaylistModalOpen && (
+          <PlaylistModal onClose={handlePlaylistModalClose} />
+        )}
+      </BackgroundWrapper>
     </>
   );
 }
