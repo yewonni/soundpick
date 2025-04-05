a45b9043ccf32b86a7de402faee58ea15e658c21
ddhsl <dnjs9199@naver.com>
2025-04-06 01:47:49 +0900

    [edit] RegisterPlaylist : 컬러 수정

--- src/pages/registerPlaylist/RegisterPlaylist.tsx
+++ src/pages/registerPlaylist/RegisterPlaylist.tsx
@@ -3,25 +3,30 @@ import FinishButton from "../../components/FinishButton";
 import { useNavigate } from "react-router-dom";
 import sample from "../../images/sample.png";
 import RegisterButton from "../../components/RegisterButton";
+import Button from "../../components/Button";
 export default function RegisterPlaylist() {
   const navigate = useNavigate();
 
   return (
     <>
-      <header className="p-4 bg-bg-sub flex justify-between">
+      <header className="p-4 bg-bg-sub flex justify-between md:justify-center md:px-[20%] md:relative">
         <img
           src={prevIcon}
           alt="이전으로 가기"
-          className="cursor-pointer"
+          className="cursor-pointer md:absolute md:left-[20%]"
           onClick={() => navigate(-1)}
         />
-        <h1 className="font-bold text-lg">새 플레이리스트 만들기</h1>
-        <FinishButton />
+        <h1 className="font-bold text-lg text-purple-900">
+          새 플레이리스트 만들기
+        </h1>
+        <div className="md:hidden">
+          <FinishButton />
+        </div>
       </header>
-      <main className="w-full min-h-screen bg-white p-4 py-7">
+      <main className="w-full min-h-screen bg-[#f5f6ff] p-4 py-7 md:px-[20%]">
         <section className="flex flex-col items-center gap-4">
           <h2 className="sr-only">플레이리스트 등록하기</h2>
-          <img src={sample} alt="" className="mb-3" />
+          <img src={sample} alt="" className="mb-3 md:w-[150px] md:h-[150px]" />
           <label htmlFor="playlistTitle" className="sr-only">
             플레이리스트 제목
           </label>
@@ -30,7 +35,7 @@ export default function RegisterPlaylist() {
             name="playlistTitle"
             type="text"
             placeholder="플레이리스트 제목"
-            className="border-b border-b-gray-300 py-2 font-bold w-full focus:outline-none"
+            className="border-b border-b-gray-300 p-2 font-bold w-full focus:outline-none"
           />
           <label htmlFor="playlistInfo" className="sr-only">
             플레이리스트 소개글
@@ -40,13 +45,14 @@ export default function RegisterPlaylist() {
             name="playlistInfo"
             type="text"
             placeholder="소개글을 입력하세요"
-            className="border-b border-b-gray-300 py-2 text-sm w-full focus:outline-none"
+            className="border-b border-b-gray-300 p-2 text-sm w-full focus:outline-none"
           />
         </section>
-        <div className="mt-5">
+        <div className="mt-5 flex justify-between items-center">
           <RegisterButton onClick={() => navigate("/search-artist")}>
             곡 추가하기
           </RegisterButton>
+          <Button outline>등록하기</Button>
         </div>
       </main>
     </>
