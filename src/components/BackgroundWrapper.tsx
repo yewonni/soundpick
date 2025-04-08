6195001c1eb451f9eb8a7702716b5a0eab002e43
ddhsl <dnjs9199@naver.com>
2025-04-08 18:56:47 +0900

    [edit] BackgroundWrapper : backgroundImage 색상 변경

--- src/components/BackgroundWrapper.tsx
+++ src/components/BackgroundWrapper.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import backImg from "../images/back-img.png";
+// import backImg from "../images/back-img.png";
 
 interface Props {
   children: React.ReactNode;
@@ -9,19 +9,18 @@ export default function BackgroundWrapper({ children }: Props) {
   return (
     <div className="relative w-full min-h-screen overflow-hidden">
       <div
-        className="absolute inset-0 z-0"
         style={{
           backgroundImage:
-            "linear-gradient(140deg, #d2e9ff 0%, #a2bbff 10%, #7b9dff 30%, #6f78f6 60%, #443d9e 100%)",
+            "linear-gradient(360deg, #6f60a5 0%, #5a5ca2 35%, #4a5a9e 60%, #3f4a7c 85%, #2e3455 100%)",
         }}
       ></div>
 
-      <div
+      {/* <div
         className="absolute inset-0 z-10 bg-cover bg-center opacity-20"
         style={{
           backgroundImage: `url(${backImg})`,
         }}
-      ></div>
+      ></div> */}
 
       <div className="relative z-20">{children}</div>
     </div>
