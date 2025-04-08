4beede790b38e92ac8b745dc3a251c6855c60662
ddhsl <dnjs9199@naver.com>
2025-04-08 19:02:05 +0900

    [edit] SelectButton : text color 변경

--- src/components/SelecteButton.tsx
+++ src/components/SelecteButton.tsx
@@ -17,7 +17,7 @@ export default function SelectButton({ children }: SelectButtonProps) {
       className={`w-full flex justify-center border border-primary p-2 md:p-3 text-md font-bold rounded-[12px] transition-colors duration-200 ${
         selected
           ? "bg-white/60 text-purple-800"
-          : "bg-transparent text-[#333] hover:bg-white/20"
+          : "bg-transparent text-text-base hover:bg-white/20"
       }`}
     >
       {children}
