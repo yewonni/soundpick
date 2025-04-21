1190b0384fe8b21e8a79854a2a48669542d27ee9
ddhsl <dnjs9199@naver.com>
2025-04-21 22:40:35 +0900

    [edit] axiosInstance : withCredentials 추가

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL_OUT,
+  baseURL: process.env.REACT_APP_API_BASE_URL,
+  withCredentials: true, //(Refresh Token 쿠키 보내기 위함)
   headers: {
     "Content-Type": "application/json",
     "ngrok-skip-browser-warning": "69420",
