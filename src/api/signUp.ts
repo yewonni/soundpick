a9f245226058349e4381ffab665e9334b2a2690f
ddhsl <dnjs9199@naver.com>
2025-04-21 22:39:36 +0900

    [del] 파일 삭제

--- src/api/signUp.ts
+++
@@ -1,6 +0,0 @@
-import axiosInstance from "./axiosInstance";
-import { JoinRequestType } from "../types/JoinFormData";
-
-export const signUp = (data: JoinRequestType) => {
-  return axiosInstance.post("/api/members/sign-up", data);
-};
