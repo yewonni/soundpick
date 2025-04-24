be8a33e4d98a5875e7fd8791ea88e3390f2519d9
ddhsl <dnjs9199@naver.com>
2025-04-24 14:49:22 +0900

    [edit] PreferenceCheckModal 컴포넌트 이동

--- src/pages/login/component/PreferenceCheckModal.tsx
+++ src/pages/login/component/PreferenceCheckModal.tsx
@@ -15,20 +15,20 @@ export default function PreferenceCheckModal({
     <Modal isOpen={isOpen} onClose={onClose}>
       <div className="flex flex-col gap-3 justify-center items-center">
         <h2 className="font-bold text-md md:text-lg">
-          취향 분석을 나중에 하실 건가요?
+          아직 취향 분석을 진행하지 않으셨어요.
         </h2>
-        <p className="text-sm md:text-base">지금은 건너뛰어도 괜찮아요!</p>
+        <p className="text-sm md:text-base">지금 바로 시작해볼까요?</p>
         <p className="text-xs text-center md:text-sm">
           <span className="text-primary text-semibold">
-            "마이페이지 &gt; 나의 활동 &gt; 음악 취향 로드맵"
+            혹은 "마이페이지 &gt; 나의 활동 &gt; 음악 취향 분석하기"
           </span>
           에서 언제든지 취향 분석을 진행할 수 있어요.
         </p>
         <div className="flex gap-2 mt-2">
-          <Button default onClick={() => navigate("/login")}>
+          <Button default onClick={() => navigate("/")}>
             건너뛰기
           </Button>
-          <Button onClick={() => navigate("/music-analysis")}>분석하기</Button>
+          <Button onClick={() => navigate("/music-analysis")}>시작하기</Button>
         </div>
       </div>
     </Modal>
