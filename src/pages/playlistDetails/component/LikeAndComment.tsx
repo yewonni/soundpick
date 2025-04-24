e2a50ad926f9d8f7794ea59ca1f571341ceee2ef
ddhsl <dnjs9199@naver.com>
2025-04-24 14:50:32 +0900

    [edit] LikeAndComment :  로그인 모달 추가

--- src/pages/playlistDetails/component/LikeAndComment.tsx
+++ src/pages/playlistDetails/component/LikeAndComment.tsx
@@ -1,19 +1,49 @@
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 import likeBtn from "../../../images/like-icon.svg";
 import reviewBtn from "../../../images/review-icon.svg";
+import { useAuth } from "../../../context/AuthContext";
+import LoginModal from "../../../components/LoginModal";
+
 export default function LikeAndComment({ isMobile }: { isMobile: boolean }) {
   const navigate = useNavigate();
+  const { isLoggedIn } = useAuth();
+  const [isOpenLoginModal, setIsOpenLoginModal] = useState(false);
+
+  const handleLike = () => {
+    if (!isLoggedIn) {
+      setIsOpenLoginModal(true);
+    }
+  };
+
+  const handleComment = () => {
+    if (!isLoggedIn) {
+      setIsOpenLoginModal(true);
+    } else {
+      navigate("/review");
+    }
+  };
+
+  const handleClose = () => {
+    setIsOpenLoginModal(false);
+  };
+
   return (
     <>
       {!isMobile && (
         <div className="hidden md:flex gap-4 items-center text-sm font-semibold mt-8">
           <div className="flex gap-1 items-center">
-            <img src={likeBtn} alt="좋아요" className="cursor-pointer" />
+            <img
+              src={likeBtn}
+              alt="좋아요"
+              className="cursor-pointer"
+              onClick={handleLike}
+            />
             <p>21,089</p>
           </div>
           <div
             className="flex gap-1 items-center cursor-pointer"
-            onClick={() => navigate("/review")}
+            onClick={handleComment}
           >
             <img src={reviewBtn} alt="댓글" />
             <p className="hover:underline">11,264</p>
@@ -23,18 +53,21 @@ export default function LikeAndComment({ isMobile }: { isMobile: boolean }) {
       {isMobile && (
         <div className="flex gap-4 items-center text-sm  font-semibold md:hidden">
           <div className="flex gap-1 items-center">
-            <img src={likeBtn} alt="좋아요" className="cursor-pointer" />
+            <img
+              src={likeBtn}
+              alt="좋아요"
+              className="cursor-pointer"
+              onClick={handleLike}
+            />
             <p>21,089</p>
           </div>
-          <div
-            className="flex gap-1 items-center "
-            onClick={() => navigate("/review")}
-          >
-            <img src={reviewBtn} alt="댓글" className="cursor-pointer " />
+          <div className="flex gap-1 items-center " onClick={handleComment}>
+            <img src={reviewBtn} alt="댓글" className="cursor-pointer" />
             <p className="cursor-pointer">11,264</p>
           </div>
         </div>
       )}
+      <LoginModal isOpen={isOpenLoginModal} onClose={handleClose} />
     </>
   );
 }
