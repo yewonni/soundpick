4db513c64ab8b609019e30e3476d2a9f90ca11b0
ddhsl <dnjs9199@naver.com>
2025-05-21 20:53:43 +0900

    [edit] Home : spotify login 필요 알림 및 취향 분석 여부에 따른 userStatus 전달

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,22 +1,16 @@
-import { useState } from "react";
+import { useState, useEffect, useMemo } from "react";
+import { useLocation } from "react-router-dom";
 import Header from "./components/Header";
 import Browsing from "./components/Browsing";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
 import sampleImg from "./images/sample.png";
 import HamburgerMenu from "./components/HamburgerMenu";
-// import BackgroundWrapper from "./components/BackgroundWrapper";
-
-const mainMockData1 = [
-  { imageSrc: sampleImg, title: "G-DRAGON", subTitle: "" },
-  { imageSrc: sampleImg, title: "NewJeans", subTitle: "" },
-  { imageSrc: sampleImg, title: "BIGBANG", subTitle: "" },
-  { imageSrc: sampleImg, title: "DAY6(데이식스)", subTitle: "" },
-  { imageSrc: sampleImg, title: "아이유", subTitle: "" },
-  { imageSrc: sampleImg, title: "BIGBANG", subTitle: "" },
-  { imageSrc: sampleImg, title: "DAY6(데이식스)", subTitle: "" },
-  { imageSrc: sampleImg, title: "아이유", subTitle: "" },
-];
+import { useAuth } from "./context/AuthContext";
+import { spotifyLogin } from "./api/login/spotifyLogin";
+import { useSearchInput } from "./context/SearchContext";
+import { UserStatus } from "./components/Browsing";
+import TasteAnalysisModal from "./components/TasteAnalysisModal";
 
 const mainMockData2 = [
   {
@@ -59,14 +53,75 @@ const mainMockData2 = [
 ];
 
 export default function Home() {
+  const location = useLocation();
+  const { accessToken, preferenceAnalyzed, isLoggedIn } = useAuth();
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isOpenTasteModal, setIsTasteModalOpen] = useState(false);
+  const { setInputValue } = useSearchInput();
 
   const handleMenuOpen = () => setIsMenuOpen(true);
   const handleMenuClose = () => setIsMenuOpen(false);
+  const handleTasteModalClose = () => {
+    setIsTasteModalOpen(false);
+  };
+
+  const spotifyLoginRequired = location.state?.spotifyLoginRequired;
+
+  const [showSpotifyAlert, setShowSpotifyAlert] = useState(false);
+
+  useEffect(() => {
+    setInputValue(""); // 홈 진입 시 검색어 초기화
+  }, [setInputValue]);
+
+  useEffect(() => {
+    if (accessToken && spotifyLoginRequired) {
+      const timer = setTimeout(() => {
+        setShowSpotifyAlert(true);
+      }, 500);
+
+      return () => clearTimeout(timer);
+    }
+  }, [accessToken, spotifyLoginRequired]);
+
+  useEffect(() => {
+    if (showSpotifyAlert) {
+      alert("Spotify 로그인이 필요합니다.");
+
+      const handleSpotifyLogin = async () => {
+        try {
+          const response = await spotifyLogin();
+          const popupUrl = response.data.data;
+          const popupOptions =
+            "width=600,height=400,scrollbars=yes,resizable=yes";
+
+          window.open(popupUrl, "_blank", popupOptions);
+        } catch (error) {
+          console.error("spotify 요청 실패", error);
+        } finally {
+          setShowSpotifyAlert(false);
+        }
+      };
+
+      handleSpotifyLogin();
+    }
+  }, [showSpotifyAlert]);
+
+  useEffect(() => {
+    if (isLoggedIn && !preferenceAnalyzed) {
+      setIsTasteModalOpen(true);
+    }
+  }, [isLoggedIn, preferenceAnalyzed]);
+
+  const tasteStatus: UserStatus = useMemo(() => {
+    return isLoggedIn
+      ? preferenceAnalyzed
+        ? "tasteDone"
+        : "noTaste"
+      : "guest";
+  }, [isLoggedIn, preferenceAnalyzed]);
 
   return (
     <>
-      {/* <BackgroundWrapper> */}
       <div
         className={` fixed z-10 inset-0 bg-black transition-opacity duration-300 md:hidden ${
           isMenuOpen ? "opacity-60 visible " : "opacity-0 invisible"
@@ -80,11 +135,14 @@ export default function Home() {
 
       <Header onClick={handleMenuOpen} />
 
-      <Browsing userStatus="guest" />
+      <Browsing userStatus={tasteStatus} />
 
-      <Main data1={mainMockData1} data2={mainMockData2} />
+      <Main data2={mainMockData2} />
       <Footer />
-      {/* </BackgroundWrapper> */}
+      <TasteAnalysisModal
+        isOpen={isOpenTasteModal}
+        onClose={handleTasteModalClose}
+      />
     </>
   );
 }
