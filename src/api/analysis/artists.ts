8051c920bf70caeacd72a0c01a745abbb7204913
ddhsl <dnjs9199@naver.com>
2025-05-21 20:28:27 +0900

    [add] 인기 아티스트 목록  api 호출 함수 추가

---
+++ src/api/analysis/artists.ts
@@ -0,0 +1,31 @@
+import axiosInstance from "../axiosInstance";
+
+interface ArtistsResponse {
+  message: string;
+  data: {
+    content: {
+      subject: string;
+      name: string;
+      imageList: {
+        url: string;
+      }[];
+    }[];
+  };
+}
+
+export const getArtists = (
+  subject: "global" | "korea",
+  page = 0,
+  size = 10
+) => {
+  return axiosInstance.get<ArtistsResponse>(
+    "/api/spotify/artist/popular-list",
+    {
+      params: {
+        page,
+        size,
+        subject,
+      },
+    }
+  );
+};
