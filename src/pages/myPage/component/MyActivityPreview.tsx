85a4451421b6dff772fa58993c1eb261acbbe378
ddhsl <dnjs9199@naver.com>
2025-05-21 20:41:11 +0900

    [edit] MyActivityPreview : 음악 취향 분석 모달 적용

--- src/pages/myPage/component/MyActivityPreview.tsx
+++ src/pages/myPage/component/MyActivityPreview.tsx
@@ -1,20 +1,31 @@
+import { useState } from "react";
 import mapIcon from "../../../images/map-icon.svg";
 import reviewList from "../../../images/review-list.svg";
 import friends from "../../../images/friends-icon.svg";
 import { useNavigate } from "react-router-dom";
+import AnalysisStartModal from "../../ musicAnalysis/component/AnalysisStartModal";
 
 interface MyActivityPreviewData {
   isMobile: boolean;
 }
 export default function MyActivityPreview({ isMobile }: MyActivityPreviewData) {
   const navigate = useNavigate();
+  const [isStartModalOpen, setIsStartModalOpen] = useState(false);
+
+  const handleStartModalClose = () => {
+    setIsStartModalOpen(false);
+  };
+
   return (
     <>
       {!isMobile && (
         <section className="bg-white shadow-lg p-6 h-full rounded-lg hidden md:block">
           <h2 className="font-bold mb-3">나의 활동</h2>
           <ul className="w-full text-sm font-semibold text-[#333]">
-            <li className="flex items-center gap-3 w-full border-b border-b-gray-200 p-3 pl-0 cursor-pointer hover:bg-gray-100">
+            <li
+              onClick={() => setIsStartModalOpen(true)}
+              className="flex items-center gap-3 w-full border-b border-b-gray-200 p-3 pl-0 cursor-pointer hover:bg-gray-100"
+            >
               <img src={mapIcon} alt="음악 취향 분석하기" />
               음악 취향 분석하기
             </li>
@@ -39,7 +50,10 @@ export default function MyActivityPreview({ isMobile }: MyActivityPreviewData) {
         <section className="bg-white shadow-lg p-6 w-full md:hidden">
           <h2 className="font-bold mb-3">나의 활동</h2>
           <ul className="w-full text-sm font-semibold text-[#333]">
-            <li className="flex items-center gap-3 w-full border-b border-b-gray-200 p-3 pl-0 cursor-pointer hover:bg-gray-100">
+            <li
+              onClick={() => setIsStartModalOpen(true)}
+              className="flex items-center gap-3 w-full border-b border-b-gray-200 p-3 pl-0 cursor-pointer hover:bg-gray-100"
+            >
               <img src={mapIcon} alt="음악 취향 분석하기" />
               음악 취향 분석하기
             </li>
@@ -60,6 +74,10 @@ export default function MyActivityPreview({ isMobile }: MyActivityPreviewData) {
           </ul>
         </section>
       )}
+      <AnalysisStartModal
+        isOpen={isStartModalOpen}
+        onClose={handleStartModalClose}
+      />
     </>
   );
 }
