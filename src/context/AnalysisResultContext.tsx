d3056a732da6ce1bd6095064803a48fcb8a10142
ddhsl <dnjs9199@naver.com>
2025-05-21 20:49:08 +0900

    [add] AnalysisResultContext 추가 : 취향 분석 결과 응답을 전역으로 관리

---
+++ src/context/AnalysisResultContext.tsx
@@ -0,0 +1,68 @@
+import { createContext, useState, ReactNode, useContext } from "react";
+
+export interface AnalysisResultType {
+  data: {
+    artistList: [
+      {
+        name: string;
+        images: [
+          {
+            url: string;
+          }
+        ];
+      }
+    ];
+    trackList: [
+      {
+        name: string;
+        album: {
+          name: string;
+          images: [
+            {
+              url: string;
+            }
+          ];
+        };
+        artists: [
+          {
+            name: string;
+          }
+        ];
+      }
+    ];
+  };
+}
+
+interface AnalysisResultContextType {
+  recommendationData: AnalysisResultType | null;
+  setRecommendationData: (data: AnalysisResultType) => void;
+}
+
+const AnalysisResultContext = createContext<
+  AnalysisResultContextType | undefined
+>(undefined);
+
+export const AnalysisResultProvider = ({
+  children,
+}: {
+  children: ReactNode;
+}) => {
+  const [recommendationData, setRecommendationData] =
+    useState<AnalysisResultType | null>(null);
+
+  return (
+    <AnalysisResultContext.Provider
+      value={{ recommendationData, setRecommendationData }}
+    >
+      {children}
+    </AnalysisResultContext.Provider>
+  );
+};
+
+export const useAnalysisResult = () => {
+  const context = useContext(AnalysisResultContext);
+  if (!context) {
+    throw new Error("usePreference must be used within PreferenceProvider");
+  }
+  return context;
+};
