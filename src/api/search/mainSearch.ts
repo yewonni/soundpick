3e043da496c6fcfffd828731adcc5df5363dc7ca
ddhsl <dnjs9199@naver.com>
2025-05-21 20:30:55 +0900

    [add] 메인 통합 검색 api 호출 함수 추가

---
+++ src/api/search/mainSearch.ts
@@ -0,0 +1,50 @@
+import axiosInstance from "../axiosInstance";
+
+interface SearchResponse {
+  message: string;
+  data: {
+    content: [
+      {
+        name: string;
+        imageList: {
+          url: string;
+        }[];
+      }
+    ];
+  };
+}
+
+interface TrackResponse {
+  message: string;
+  data: {
+    content: [
+      {
+        name: string;
+        imageList: {
+          url: string;
+        }[];
+        trackArtistNameList: string;
+      }
+    ];
+  };
+}
+
+export const trackSearch = (page = 0, size = 10, query = "") => {
+  return axiosInstance.get<TrackResponse>("/api/spotify/search/track", {
+    params: {
+      page,
+      size,
+      query,
+    },
+  });
+};
+
+export const playlistSearch = (page = 0, size = 10, query = "") => {
+  return axiosInstance.get<SearchResponse>("/api/spotify/search/playlist", {
+    params: {
+      page,
+      size,
+      query,
+    },
+  });
+};
