70f14e1f6db7214001a0244e579e45b649248398
ddhsl <dnjs9199@naver.com>
2025-05-21 20:23:03 +0900

    [del] lastfm.ts 파일 삭제

--- src/api/admin/lastfm.ts
+++
@@ -1,9 +0,0 @@
-import axiosInstance from "../axiosInstance";
-
-interface getLastFmResponse {
-  data: string;
-}
-
-export const getLastFm = () => {
-  return axiosInstance.get<getLastFmResponse>("/api/lastfm/login");
-};
