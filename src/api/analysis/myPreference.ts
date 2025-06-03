8723040ab0a2a66358a375b1edb3473e19eb8fa8
ddhsl <dnjs9199@naver.com>
2025-06-03 14:55:23 +0900

    [add] myPreference : 추천 트랙/아티스트 추가 및 삭제 api 함수 구현

--- src/api/analysis/myPreference.ts
+++ src/api/analysis/myPreference.ts
@@ -9,8 +9,10 @@ export interface AnalysisResultType {
   data: {
     artistList: [
       {
+        spotifyArtistId: string;
+        recommendArtistSeq: string;
         name: string;
-        images: [
+        imageList: [
           {
             url: string;
           }
@@ -19,18 +21,17 @@ export interface AnalysisResultType {
     ];
     trackList: [
       {
+        spotifyTrackId: string;
+        recommendTrackSeq: string;
         name: string;
-        album: {
-          name: string;
-          images: [
-            {
-              url: string;
-            }
-          ];
-        };
-        artists: [
+        trackArtists: [
           {
             name: string;
+          }
+        ];
+        imageList: [
+          {
+            url: string;
           }
         ];
       }
@@ -38,9 +39,52 @@ export interface AnalysisResultType {
   };
 }
 
+// 취향 분석 단계 - 선택
 export const myPreference = (data: MyPreferenceType) => {
   return axiosInstance.post<AnalysisResultType>(
     "/api/spotify/recommendation/user",
     data
   );
+};
+
+// 곡 추천 결과 수정(추가)
+interface EditTrackType {
+  spotifyTrackSeq: string;
+}
+export const editTrackList = (data: EditTrackType[]) => {
+  return axiosInstance.post("/api/spotify/recommendation/track", data);
+};
+
+// 곡 추천 결과 삭제
+interface DeleteTrackType {
+  recommendTrackSeq: string;
+}
+
+export const deleteTrack = (data: DeleteTrackType[]) => {
+  return axiosInstance.request({
+    url: "/api/spotify/recommendation/track",
+    method: "delete",
+    data,
+  });
+};
+
+// 아티스트 결과 수정(추가)
+interface EditArtistType {
+  spotifyArtistSeq: string;
+}
+export const editArtistList = (data: EditArtistType[]) => {
+  return axiosInstance.post("/api/spotify/recommendation/artist", data);
+};
+
+// 아티스트 추천 결과 삭제
+interface DeleteArtistType {
+  recommendArtistSeq: string;
+}
+
+export const deleteArtist = (data: DeleteArtistType[]) => {
+  return axiosInstance.request({
+    url: "/api/spotify/recommendation/artist",
+    method: "delete",
+    data,
+  });
 };
