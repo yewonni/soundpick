2999425da1151f28eccd0332ffbc1302658397c4
ddhsl <dnjs9199@naver.com>
2025-06-03 15:16:06 +0900

    [edit] Main Title 상수 추가

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -2,3 +2,8 @@ export const SECTION_TITLES = {
   POPULAR_ARTISTS: "인기 아티스트",
   RECOMMENDED_PLAYLISTS: "추천 플레이리스트 🌷",
 } as const;
+
+export const RECOMMENDED_SECTION_TITLES = {
+  TASTE_MATCHING_MUSIC: "님 취향 저격 음악 🎵",
+  CUSTOM_PLAYLISTS_FOR_YOU: "님을 위한, 맞춤 플리 🌈",
+} as const;
