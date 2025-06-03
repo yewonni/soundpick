b653068fbcd7fdc75f4ced19b92491b23116aff8
ddhsl <dnjs9199@naver.com>
2025-06-03 15:43:06 +0900

    [edit] LikeAndComment : 좋아요 기능 적용

--- src/pages/playlistDetails/component/LikeAndComment.tsx
+++ src/pages/playlistDetails/component/LikeAndComment.tsx
@@ -1,73 +1,83 @@
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 import likeBtn from "../../../images/like-icon.svg";
+import likeFull from "../../../images/likeFull.svg";
 import reviewBtn from "../../../images/review-icon.svg";
-import { useAuth } from "../../../context/AuthContext";
-import LoginModal from "../../../components/LoginModal";
+import {
+  likePlaylist,
+  deleteLike,
+} from "../../../api/playlistDetails/playlistDetails";
+import LikePlaylistModal from "./LikePlaylistModal";
+import { useLikeContext } from "../../../context/LIkeContext";
+import { PlaylistData } from "../PlaylistDetails";
 
-export default function LikeAndComment({ isMobile }: { isMobile: boolean }) {
+export default function LikeAndComment({
+  isMobile,
+  playlistSeq,
+  playlistData,
+}: {
+  isMobile: boolean;
+  playlistSeq: string | undefined;
+  playlistData?: PlaylistData | null;
+}) {
   const navigate = useNavigate();
-  const { isLoggedIn } = useAuth();
-  const [isOpenLoginModal, setIsOpenLoginModal] = useState(false);
+  const [isOpenModal, setIsModalOpen] = useState(false);
+  const { isLiked, likeCount, toggleLike } = useLikeContext();
 
-  const handleLike = () => {
-    if (!isLoggedIn) {
-      setIsOpenLoginModal(true);
+  const handleLike = async () => {
+    if (!playlistSeq) return;
+    try {
+      if (isLiked) {
+        await deleteLike([{ spotifyPlaylistSeq: playlistSeq }]);
+        alert("저장된 플레이리스트가 삭제되었습니다.");
+      } else {
+        await likePlaylist(playlistSeq);
+        setIsModalOpen(true);
+      }
+
+      toggleLike();
+    } catch (error) {
+      console.error("좋아요 처리 실패", error);
     }
   };
 
   const handleComment = () => {
-    if (!isLoggedIn) {
-      setIsOpenLoginModal(true);
-    } else {
-      navigate("/review");
+    if (playlistSeq) {
+      navigate(`/review/${playlistSeq}`);
     }
-  };
-
-  const handleClose = () => {
-    setIsOpenLoginModal(false);
   };
 
   return (
     <>
-      {!isMobile && (
-        <div className="hidden md:flex gap-4 items-center text-sm font-semibold mt-8">
-          <div className="flex gap-1 items-center">
-            <img
-              src={likeBtn}
-              alt="좋아요"
-              className="cursor-pointer"
-              onClick={handleLike}
-            />
-            <p>21,089</p>
-          </div>
-          <div
-            className="flex gap-1 items-center cursor-pointer"
-            onClick={handleComment}
-          >
-            <img src={reviewBtn} alt="댓글" />
-            <p className="hover:underline">11,264</p>
-          </div>
+      <div
+        className={`flex gap-4 items-center text-sm font-semibold ${
+          isMobile ? "md:hidden" : "hidden md:flex mt-8"
+        }`}
+      >
+        <div className="flex gap-1 items-center">
+          <img
+            src={isLiked ? likeFull : likeBtn}
+            alt="좋아요"
+            className="cursor-pointer"
+            onClick={handleLike}
+          />
+          <p>{likeCount.toLocaleString()}</p>
         </div>
-      )}
-      {isMobile && (
-        <div className="flex gap-4 items-center text-sm  font-semibold md:hidden">
-          <div className="flex gap-1 items-center">
-            <img
-              src={likeBtn}
-              alt="좋아요"
-              className="cursor-pointer"
-              onClick={handleLike}
-            />
-            <p>21,089</p>
-          </div>
-          <div className="flex gap-1 items-center " onClick={handleComment}>
-            <img src={reviewBtn} alt="댓글" className="cursor-pointer" />
-            <p className="cursor-pointer">11,264</p>
-          </div>
+        <div
+          className="flex gap-1 items-center cursor-pointer"
+          onClick={handleComment}
+        >
+          <img src={reviewBtn} alt="댓글" />
+          <p className={`${!isMobile ? "hover:underline" : ""}`}>
+            {playlistData?.reviewCount}
+          </p>
         </div>
-      )}
-      <LoginModal isOpen={isOpenLoginModal} onClose={handleClose} />
+      </div>
+
+      <LikePlaylistModal
+        isOpen={isOpenModal}
+        onClose={() => setIsModalOpen(false)}
+      />
     </>
   );
 }
