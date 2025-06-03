a335c17ee9dde6f83e6ed1f73fd646e58a0cf6e3
ddhsl <dnjs9199@naver.com>
2025-06-03 15:46:25 +0900

    [edit] RegisterPlaylist : 나의 플레이리스트 등록하기 api 적용

--- src/pages/registerPlaylist/RegisterPlaylist.tsx
+++ src/pages/registerPlaylist/RegisterPlaylist.tsx
@@ -1,11 +1,86 @@
+import { useState, useRef } from "react";
 import prevIcon from "../../images/chevron-left.svg";
 import FinishButton from "../../components/FinishButton";
 import { useNavigate } from "react-router-dom";
-import sample from "../../images/sample.png";
-import RegisterButton from "../../components/RegisterButton";
+import sample from "../../images/playlistSample.svg";
 import Button from "../../components/Button";
+import {
+  registerMyPlaylist,
+  uploadMyPlaylistImage,
+} from "../../api/myPage/myPlaylist";
 export default function RegisterPlaylist() {
   const navigate = useNavigate();
+  const fileInputRef = useRef<HTMLInputElement | null>(null);
+  const [playlistName, setPlaylistName] = useState("");
+  const [playlistDescription, setPlaylistDescription] = useState("");
+  const [isDirty, setIsDirty] = useState(false);
+  const [preview, setPreview] = useState(sample);
+  const [file, setFile] = useState<File | null>(null);
+  const [objectUrl, setObjectUrl] = useState<string | null>(null);
+
+  const handleImageClick = () => {
+    fileInputRef.current?.click();
+  };
+
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const selectedFile = e.target.files?.[0];
+    if (!selectedFile) return;
+
+    if (objectUrl) {
+      URL.revokeObjectURL(objectUrl);
+    }
+
+    const imageUrl = URL.createObjectURL(selectedFile);
+    setPreview(imageUrl);
+    setObjectUrl(imageUrl);
+    setFile(selectedFile);
+    setIsDirty(true);
+  };
+
+  const handlePlaylistRegister = async () => {
+    if (!playlistName.trim()) {
+      alert("플레이리스트 제목을 입력해주세요.");
+      return;
+    }
+    if (!playlistDescription.trim()) {
+      alert("플레이리스트 소개글을 입력해주세요.");
+      return;
+    }
+    if (!file) {
+      alert("플레이리스트 이미지를 등록해주세요.");
+      return;
+    }
+
+    try {
+      const response = await registerMyPlaylist({
+        name: playlistName,
+        description: playlistDescription,
+      });
+
+      const spotifyPlaylistId = response.data.data;
+
+      if (spotifyPlaylistId) {
+        await uploadMyPlaylistImage(file, spotifyPlaylistId);
+      }
+
+      alert("성공적으로 등록되었습니다.");
+      setIsDirty(false);
+    } catch (error) {
+      console.error("플리 등록 실패", error);
+      alert("플레이리스트 등록에 실패했습니다.");
+    }
+  };
+
+  const handleBack = () => {
+    if (isDirty) {
+      const confirmLeave = window.confirm("저장하지 않고 나가시겠습니까?");
+      if (confirmLeave) {
+        navigate(-1);
+      }
+    } else {
+      navigate(-1);
+    }
+  };
 
   return (
     <>
@@ -14,21 +89,38 @@ export default function RegisterPlaylist() {
           src={prevIcon}
           alt="이전으로 가기"
           className="cursor-pointer md:absolute md:left-[20%]"
-          onClick={() => navigate(-1)}
+          onClick={handleBack}
         />
         <h1 className="font-bold text-lg ">새 플레이리스트 만들기</h1>
         <div className="md:hidden">
-          <FinishButton />
+          <FinishButton onClick={handleBack} />
         </div>
       </header>
       <main className="w-full min-h-screen bg-bg-peach p-4 py-7 md:px-[20%]">
         <section className="flex flex-col items-center gap-4">
           <h2 className="sr-only">플레이리스트 등록하기</h2>
-          <img src={sample} alt="" className="mb-3 md:w-[150px] md:h-[150px]" />
+          <img
+            onClick={handleImageClick}
+            src={preview}
+            alt="플레이리스트 이미지"
+            className="w-[150px] h-[150px] object-cover rounded-md cursor-pointer mb-3"
+          />
+
+          <input
+            type="file"
+            accept="image/*"
+            className="hidden"
+            ref={fileInputRef}
+            onChange={handleFileChange}
+          />
           <label htmlFor="playlistTitle" className="sr-only">
             플레이리스트 제목
           </label>
           <input
+            onChange={(e) => {
+              setPlaylistName(e.target.value);
+              setIsDirty(true);
+            }}
             id="playlistTitle"
             name="playlistTitle"
             type="text"
@@ -39,6 +131,10 @@ export default function RegisterPlaylist() {
             플레이리스트 소개글
           </label>
           <input
+            onChange={(e) => {
+              setPlaylistDescription(e.target.value);
+              setIsDirty(true);
+            }}
             id="playlistInfo"
             name="playlistInfo"
             type="text"
@@ -46,11 +142,10 @@ export default function RegisterPlaylist() {
             className="p-2 text-sm w-full focus:outline-none rounded-md bg-white/60"
           />
         </section>
-        <div className="mt-5 flex justify-between items-center">
-          <RegisterButton onClick={() => navigate("/search-artist")}>
-            곡 추가하기
-          </RegisterButton>
-          <Button primary>등록하기</Button>
+        <div className="mt-5 flex justify-end">
+          <Button primary onClick={handlePlaylistRegister}>
+            등록하기
+          </Button>
         </div>
       </main>
     </>
