48810e7cabf70ecf36c4d3e5dd3a17e8f6c4e5b7
ddhsl <dnjs9199@naver.com>
2025-06-03 15:35:33 +0900

    [edit] LikedPlaylistsPreview : 좋아하는 플레이리스트 목록 api 적용

--- src/pages/myPage/component/LikedPlaylists.tsx
+++ src/pages/myPage/component/LikedPlaylists.tsx
@@ -1,24 +1,40 @@
+import { useEffect, useState } from "react";
 import prevIcon from "../../../images/chevron-left.svg";
 import { useNavigate } from "react-router-dom";
-import sample from "../../../images/sample.png";
+import catImg from "../../../images/music-cat-full.png";
+import { getLikedPlaylists } from "../../../api/myPage/likedPlaylists";
+import { useLoading } from "../../../context/LoadingContext";
+
+interface PlaylistsType {
+  preferencePlaylistSeq: string;
+  spotifyPlaylistSeq: string;
+  spotifyPlaylistId: string;
+  name: string;
+  description: string;
+  owner: string;
+  imageList: [
+    {
+      url: string;
+    }
+  ];
+}
 export default function LikedPlaylists() {
   const navigate = useNavigate();
+  const { loading } = useLoading();
+  const [playlists, setPlaylists] = useState<PlaylistsType[]>([]);
 
-  const mockData = [
-    {
-      imageUrl: sample,
-      title: "설거지 10분 만에 하는 노래 ",
-    },
-    {
-      imageUrl: sample,
-      title: "새벽감성이 찾아올 때 듣는 노래",
-    },
-    {
-      imageUrl: sample,
-      title: "쇠맛나는 k-pop 플리 모음집",
-    },
-  ];
+  const fetchLikedPlaylists = async () => {
+    try {
+      const res = await getLikedPlaylists();
+      setPlaylists(res.data.data.content);
+    } catch (error) {
+      console.log("플레이리스트 불러오기 실패", error);
+    }
+  };
 
+  useEffect(() => {
+    fetchLikedPlaylists();
+  }, []);
   return (
     <>
       <header className="p-4 bg-bg-sub flex justify-center items-center relative md:px-[20%]">
@@ -31,22 +47,34 @@ export default function LikedPlaylists() {
         <h1 className="font-bold text-lg ">내가 좋아한 플레이리스트</h1>
       </header>
       <main className="w-full min-h-screen px-4 py-2 md:px-[20%]">
-        {mockData.map((data, index) => (
-          <article
-            key={index}
-            className="py-3 border-b border-b-gray-200 cursor-pointer flex gap-4 items-center"
-            onClick={() => navigate("/liked-playlist-details")}
-          >
-            <img
-              src={data.imageUrl}
-              alt={data.title}
-              className="w-[60px] h-[60px] rounded-sm md:w-[120px] md:h-[120px]"
-            />
-            <h2 className="font-bold  md:hover:underline text-white ">
-              {data.title}
-            </h2>
-          </article>
-        ))}
+        {!loading && playlists.length === 0 ? (
+          <div className="text-center text-sm md:text-text-lg text-gray-200 mt-20 flex flex-col gap-2">
+            <p>플레이리스트 목록이 없습니다.</p>
+            <p>마음껏 플레이리스트를 저장해보세요!</p>
+          </div>
+        ) : (
+          playlists.map((data, index) => (
+            <article
+              key={index}
+              className="py-3 border-b border-b-gray-200 cursor-pointer flex gap-4 items-center"
+              onClick={() =>
+                navigate(`/playlist-details/${data.spotifyPlaylistSeq}`)
+              }
+            >
+              <img
+                src={data.imageList[0]?.url || catImg}
+                alt={data.name}
+                className="w-[60px] h-[60px] rounded-sm md:w-[120px] md:h-[120px]"
+              />
+              <div className="flex flex-col gap-2">
+                <h2 className="font-bold  md:hover:underline text-white text-sm md:text-base ">
+                  {data.name}
+                </h2>
+                <p className="text-gray-200 text-xs md:text-sm">{data.owner}</p>
+              </div>
+            </article>
+          ))
+        )}
       </main>
     </>
   );
