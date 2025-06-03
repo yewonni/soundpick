391e100e8d4bfea249d380ad92adc82e83737983
ddhsl <dnjs9199@naver.com>
2025-06-03 14:50:51 +0900

    [del] Home : mock data 제거

--- src/Home.tsx
+++ src/Home.tsx
@@ -4,53 +4,12 @@ import Header from "./components/Header";
 import Browsing from "./components/Browsing";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
-import sampleImg from "./images/sample.png";
 import HamburgerMenu from "./components/HamburgerMenu";
 import { useAuth } from "./context/AuthContext";
 import { spotifyLogin } from "./api/login/spotifyLogin";
 import { useSearchInput } from "./context/SearchContext";
 import { UserStatus } from "./components/Browsing";
 import TasteAnalysisModal from "./components/TasteAnalysisModal";
-
-const mainMockData2 = [
-  {
-    imageSrc: sampleImg,
-    title: "하루종일 듣고싶은 감성 R&B 모음집",
-    subTitle: "",
-  },
-  {
-    imageSrc: sampleImg,
-    title: "지루한 일상을 벗어나고 싶을 때 들어봐!",
-    subTitle: "",
-  },
-  {
-    imageSrc: sampleImg,
-    title: "상쾌한 아침을 맞이하기 위한 플리",
-    subTitle: "",
-  },
-  {
-    imageSrc: sampleImg,
-    title: "다가오는 여름 맞이 플레이리스트",
-    subTitle: "",
-  },
-  { imageSrc: sampleImg, title: "봄 내음 물씬 나는 노래 모음집", subTitle: "" },
-  {
-    imageSrc: sampleImg,
-    title: "지루한 일상을 벗어나고 싶을 때 들어봐!",
-    subTitle: "",
-  },
-  {
-    imageSrc: sampleImg,
-    title: "상쾌한 아침을 맞이하기 위한 플리",
-    subTitle: "",
-  },
-  {
-    imageSrc: sampleImg,
-    title: "다가오는 여름 맞이 플레이리스트",
-    subTitle: "",
-  },
-  { imageSrc: sampleImg, title: "봄 내음 물씬 나는 노래 모음집", subTitle: "" },
-];
 
 export default function Home() {
   const location = useLocation();
@@ -70,7 +29,7 @@ export default function Home() {
   const [showSpotifyAlert, setShowSpotifyAlert] = useState(false);
 
   useEffect(() => {
-    setInputValue(""); // 홈 진입 시 검색어 초기화
+    setInputValue("");
   }, [setInputValue]);
 
   useEffect(() => {
@@ -137,7 +96,7 @@ export default function Home() {
 
       <Browsing userStatus={tasteStatus} />
 
-      <Main data2={mainMockData2} />
+      <Main />
       <Footer />
       <TasteAnalysisModal
         isOpen={isOpenTasteModal}
