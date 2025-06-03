d9cdce191d946909d3d5ccf17ba7ab6f4aff98d4
ddhsl <dnjs9199@naver.com>
2025-06-03 14:49:53 +0900

    [add] App : 라우터 및 리덕스 provider , store 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import React from "react";
 import { useEffect } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import { Provider } from "react-redux";
+import store from "./store/index";
 import Home from "./Home";
 import Search from "./pages/search/Search";
 import SearchResult from "./pages/searchResult/SearchResult";
@@ -12,24 +13,32 @@ import JoinSuccess from "./pages/join/JoinSuccess";
 import MusicAnalysis from "./pages/ musicAnalysis/ MusicSurvey";
 import ArtistSearch from "./pages/ musicAnalysis/component/ArtistSearch";
 import Recommendation from "./pages/ musicAnalysis/component/Recommendation";
+import EditArtistSearch from "./pages/ musicAnalysis/component/EditArtistSearch";
+import EditTrackSearch from "./pages/ musicAnalysis/component/EditTrackSearch";
 import Review from "./pages/review/Review";
 import MyPage from "./pages/myPage/MyPage";
 import MyAllTimeHits from "./pages/myPage/component/MyAllTimeHits";
+import MyAllTimeTrackSearch from "./pages/myPage/component/MyAllTimeTrackSearch";
 import MyPlaylist from "./pages/myPage/component/MyPlaylist";
 import MyPlaylistDetails from "./pages/myPlaylistDetails/MyPlaylistDetails";
 import LikedPlaylists from "./pages/myPage/component/LikedPlaylists";
-import LikedPlaylistDetails from "./pages/likedPlaylistDetails/LikedPlaylistDetails";
 import ErrorPage from "./pages/404error/ErrorPage";
 import RegisterPlaylist from "./pages/registerPlaylist/RegisterPlaylist";
 import MyReview from "./pages/myReview/MyReview";
 import MyFriends from "./pages/myFriends/MyFriends";
+import FindFriends from "./pages/findFriends/FindFriends";
 import EditProfile from "./pages/editProfile/EditProfile";
 import PopularArtistsPage from "./pages/popularArtists/PopularArtistsPage";
-import RecommendedPlaylistsPage from "./pages/recommendedPlaylists/RecommendedPlaylistsPage";
+import PopularPlaylistsPage from "./pages/popularPlaylists/PopularPlaylistsPage";
+import RecommendedPlaylistPage from "./pages/recommendedPlaylist/RecommendedPlaylistPage";
+import RecommendedTrackPage from "./pages/recommendedTrack/RecommendedTrackPage";
 import { AuthProvider, useAuth } from "./context/AuthContext";
 import { injectSetAccessToken } from "./api/axiosInstance";
 import { SearchProvider } from "./context/SearchContext";
 import { AnalysisResultProvider } from "./context/AnalysisResultContext";
+import { TrackSelectionProvider } from "./context/TrackSelectionContext";
+import { ArtistSelectionProvider } from "./context/ArtistSelectionContext";
+import { ArtistAnalysisProvider } from "./context/ArtistAnalysisContext";
 // import PrivateRoute from "./components/PrivateRouter";
 import axiosInstance, {
   RefreshResponse,
@@ -40,18 +49,26 @@ import { LoadingProvider, useLoading } from "./context/LoadingContext";
 
 const App: React.FC = () => {
   return (
-    <AuthProvider>
-      <LoadingProvider>
-        <SearchProvider>
-          <AnalysisResultProvider>
-            <BrowserRouter>
-              <AppInitializer />
-              <AppContent />
-            </BrowserRouter>
-          </AnalysisResultProvider>
-        </SearchProvider>
-      </LoadingProvider>
-    </AuthProvider>
+    <Provider store={store}>
+      <AuthProvider>
+        <LoadingProvider>
+          <SearchProvider>
+            <AnalysisResultProvider>
+              <TrackSelectionProvider>
+                <ArtistSelectionProvider>
+                  <ArtistAnalysisProvider>
+                    <BrowserRouter>
+                      <AppInitializer />
+                      <AppContent />
+                    </BrowserRouter>
+                  </ArtistAnalysisProvider>
+                </ArtistSelectionProvider>
+              </TrackSelectionProvider>
+            </AnalysisResultProvider>
+          </SearchProvider>
+        </LoadingProvider>
+      </AuthProvider>
+    </Provider>
   );
 };
 
@@ -64,48 +81,49 @@ function AppContent() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/popular-artists" element={<PopularArtistsPage />} />
-        <Route
-          path="/recommended-playlists"
-          element={<RecommendedPlaylistsPage />}
-        />
-        <Route path="/search" element={<Search />} />
-        <Route path="/search-result" element={<SearchResult />} />
-        <Route path="/playlist-details" element={<PlaylistDetails />} />
+        <Route path="/popular-playlists" element={<PopularPlaylistsPage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/join" element={<Join />} />
         <Route path="/join-success" element={<JoinSuccess />} />
+        <Route path="/*" element={<ErrorPage />} />
         {/* PrivateRoute로 보호된 라우트들 */}
-        <Route path="/review" element={<Review />} />
+        <Route
+          path="/recommended-playlists"
+          element={<RecommendedPlaylistPage />}
+        />
+        <Route path="/recommended-tracks" element={<RecommendedTrackPage />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/search-result" element={<SearchResult />} />
+        <Route path="/playlist-details/:seq" element={<PlaylistDetails />} />
+        <Route path="/review/:spotifyPlaylistSeq" element={<Review />} />
         <Route path="/music-analysis" element={<MusicAnalysis />} />
         <Route path="/search-artist" element={<ArtistSearch />} />
         <Route path="/recommendation" element={<Recommendation />} />
+        <Route path="/edit-artist" element={<EditArtistSearch />} />
+        <Route path="/edit-track" element={<EditTrackSearch />} />
         <Route path="/mypage" element={<MyPage />} />
         <Route path="/my-hits" element={<MyAllTimeHits />} />
+        <Route path="/my-hits-search" element={<MyAllTimeTrackSearch />} />
         <Route path="/my-playlist" element={<MyPlaylist />} />
-        <Route path="/my-playlist-details" element={<MyPlaylistDetails />} />
+        <Route
+          path="/my-playlist-details/:seq"
+          element={<MyPlaylistDetails />}
+        />
         <Route path="/register-playlist" element={<RegisterPlaylist />} />
         <Route path="/liked-playlist" element={<LikedPlaylists />} />
-        <Route
-          path="/liked-playlist-details"
-          element={<LikedPlaylistDetails />}
-        />
         <Route path="/my-review" element={<MyReview />} />
         <Route path="/my-friends" element={<MyFriends />} />
+        <Route path="/find-friends" element={<FindFriends />} />
         <Route path="/edit-profile" element={<EditProfile />} />
-        <Route path="/*" element={<ErrorPage />} />
       </Routes>
     </>
   );
 }
 
 function AppInitializer() {
-  const {
-    setAccessToken,
-    setIsLoggedIn,
-    setPreferenceAnalyzed,
-    preferenceAnalyzed,
-  } = useAuth();
-  const { setLoading } = useLoading(); // useLoading 훅 사용
+  const { setAccessToken, setIsLoggedIn, setHasInitialized, setUserNickname } =
+    useAuth();
+  const { setLoading } = useLoading();
 
   useEffect(() => {
     // axiosInstance에 setAccessToken 함수 주입
@@ -115,9 +133,13 @@ function AppInitializer() {
     injectSetLoading(setLoading);
 
     // 페이지 로드 시 자동으로 토큰 갱신 시도
+    let isVerifying = false;
+
     async function verifyTokenOnLoad() {
+      if (isVerifying) return;
+      isVerifying = true;
+
       try {
-        // 저장된 토큰이 있는지 검증하는 API 호출
         const res = await axiosInstance.post<RefreshResponse>(
           `/api/auth/refresh`,
           {},
@@ -125,23 +147,23 @@ function AppInitializer() {
         );
 
         const newAccessToken = res.data.data.accessToken;
-        const updatedPreference = res.data.meta.preferenceAnalyzed;
+        const updatedUserNickname = res.data.data.nickname;
 
-        // 새 토큰으로 헤더 업데이트
         axiosInstance.defaults.headers.common[
           "Authorization"
         ] = `Bearer ${newAccessToken}`;
 
-        // AuthContext 상태 업데이트
         setAccessToken(newAccessToken);
         setIsLoggedIn(true);
-        setPreferenceAnalyzed(updatedPreference);
+        setUserNickname(updatedUserNickname);
       } catch (error) {
         console.error("자동 로그인 실패:", error);
-        // 현재 위치가 로그인 페이지가 아닐 때만 리다이렉트
         if (window.location.pathname !== "/login") {
           window.location.href = "/login";
         }
+      } finally {
+        isVerifying = false;
+        setHasInitialized(true);
       }
     }
 
@@ -154,8 +176,8 @@ function AppInitializer() {
     setAccessToken,
     setIsLoggedIn,
     setLoading,
-    setPreferenceAnalyzed,
-    preferenceAnalyzed,
+    setUserNickname,
+    setHasInitialized,
   ]);
 
   return null;
