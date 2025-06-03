f38d863dde70e07e473eb05cd515ba868507df43
ddhsl <dnjs9199@naver.com>
2025-06-03 15:43:21 +0900

    [add] LikePlaylistModal 컴포넌트 추가

---
+++ src/pages/playlistDetails/component/LikePlaylistModal.tsx
@@ -0,0 +1,31 @@
+import Modal from "../../../components/Modal";
+import Button from "../../../components/Button";
+import { useNavigate } from "react-router-dom";
+
+interface LikePlaylistModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+}
+export default function LikePlaylistModal({
+  isOpen,
+  onClose,
+}: LikePlaylistModalProps) {
+  const navigate = useNavigate();
+
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <div className="flex flex-col gap-1 items-center">
+        <p className="font-bold">플레이리스트 저장 완료!</p>
+        <p className="text-[#333] text-xs md:text-sm mb-2">
+          마이페이지에서 언제든지 다시 들을 수 있어요 🎧
+        </p>
+        <div className="flex gap-2 mt-3">
+          <Button default onClick={onClose}>
+            닫기
+          </Button>
+          <Button onClick={() => navigate("/mypage")}>이동하기</Button>
+        </div>
+      </div>
+    </Modal>
+  );
+}
