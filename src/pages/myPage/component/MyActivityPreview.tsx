a8865edcb2314c83a7c10e7c410bbd57fd4c063e
ddhsl <dnjs9199@naver.com>
2025-06-11 19:24:31 +0900

    [edit] MyActivityPreview : 모바일 / 데스크탑 UI 구조 리팩토링

--- src/pages/myPage/component/MyActivityPreview.tsx
+++ src/pages/myPage/component/MyActivityPreview.tsx
@@ -8,6 +8,7 @@ import AnalysisStartModal from "../../ musicAnalysis/component/AnalysisStartModa
 interface MyActivityPreviewData {
   isMobile: boolean;
 }
+
 export default function MyActivityPreview({ isMobile }: MyActivityPreviewData) {
   const navigate = useNavigate();
   const [isStartModalOpen, setIsStartModalOpen] = useState(false);
@@ -16,64 +17,51 @@ export default function MyActivityPreview({ isMobile }: MyActivityPreviewData) {
     setIsStartModalOpen(false);
   };
 
+  const activityList = [
+    {
+      icon: mapIcon,
+      text: "음악 취향 분석하기",
+      onClick: () => setIsStartModalOpen(true),
+    },
+    {
+      icon: reviewList,
+      text: "내가 남긴 한 마디",
+      onClick: () => navigate("/my-review"),
+    },
+    {
+      icon: friends,
+      text: "나의 음악 친구",
+      onClick: () => navigate("/my-friends"),
+    },
+  ];
+
+  function ActivityList() {
+    return (
+      <ul className="w-full text-sm font-semibold text-[#333]">
+        {activityList.map(({ icon, text, onClick }) => (
+          <li
+            key={text}
+            onClick={onClick}
+            className="flex items-center gap-3 w-full border-b border-b-gray-200 p-3 pl-0 cursor-pointer hover:bg-gray-100"
+          >
+            <img src={icon} alt={text} />
+            {text}
+          </li>
+        ))}
+      </ul>
+    );
+  }
+
   return (
     <>
-      {!isMobile && (
-        <section className="bg-white shadow-lg p-6 h-full rounded-lg hidden md:block">
-          <h2 className="font-bold mb-3">나의 활동</h2>
-          <ul className="w-full text-sm font-semibold text-[#333]">
-            <li
-              onClick={() => setIsStartModalOpen(true)}
-              className="flex items-center gap-3 w-full border-b border-b-gray-200 p-3 pl-0 cursor-pointer hover:bg-gray-100"
-            >
-              <img src={mapIcon} alt="음악 취향 분석하기" />
-              음악 취향 분석하기
-            </li>
-            <li
-              onClick={() => navigate("/my-review")}
-              className="flex items-center gap-3 w-full border-b border-b-gray-200 p-3 pl-0 cursor-pointer hover:bg-gray-100"
-            >
-              <img src={reviewList} alt="내가 남긴 한 마디" />
-              내가 남긴 한 마디
-            </li>
-            <li
-              onClick={() => navigate("/my-friends")}
-              className="flex items-center gap-3 w-full border-b border-b-gray-200 p-3 pl-0 cursor-pointer hover:bg-gray-100"
-            >
-              <img src={friends} alt="나의 음악 친구" />
-              나의 음악 친구
-            </li>
-          </ul>
-        </section>
-      )}
-      {isMobile && (
-        <section className="bg-white shadow-lg p-6 w-full md:hidden">
-          <h2 className="font-bold mb-3">나의 활동</h2>
-          <ul className="w-full text-sm font-semibold text-[#333]">
-            <li
-              onClick={() => setIsStartModalOpen(true)}
-              className="flex items-center gap-3 w-full border-b border-b-gray-200 p-3 pl-0 cursor-pointer hover:bg-gray-100"
-            >
-              <img src={mapIcon} alt="음악 취향 분석하기" />
-              음악 취향 분석하기
-            </li>
-            <li
-              onClick={() => navigate("/my-review")}
-              className="flex items-center gap-3 w-full border-b border-b-gray-200 p-3 pl-0 cursor-pointer hover:bg-gray-100"
-            >
-              <img src={reviewList} alt="내가 남긴 한 마디" />
-              내가 남긴 한 마디
-            </li>
-            <li
-              onClick={() => navigate("/my-friends")}
-              className="flex items-center gap-3 w-full border-b border-b-gray-200 p-3 pl-0 cursor-pointer hover:bg-gray-100"
-            >
-              <img src={friends} alt="나의 음악 친구" />
-              나의 음악 친구
-            </li>
-          </ul>
-        </section>
-      )}
+      <section
+        className={`bg-white shadow-lg p-6 ${
+          isMobile ? "w-full md:hidden" : "h-full rounded-lg hidden md:block"
+        }`}
+      >
+        <h2 className="font-bold mb-3">나의 활동</h2>
+        <ActivityList />
+      </section>
       <AnalysisStartModal
         isOpen={isStartModalOpen}
         onClose={handleStartModalClose}
