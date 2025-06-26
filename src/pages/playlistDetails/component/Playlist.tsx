f895a8222851a9411f26adfa4dc4870cdb6c351c
ddhsl <dnjs9199@naver.com>
2025-06-26 21:43:07 +0900

    [edit] Playlist : 트랙 없는 경우 메세지 추가

--- src/pages/playlistDetails/component/Playlist.tsx
+++ src/pages/playlistDetails/component/Playlist.tsx
@@ -58,77 +58,94 @@ export default function Playlist({ isMobile }: PlaylistProps) {
       {error && (
         <p className="text-primary">트랙 목록을 불러오는 데 실패했습니다.</p>
       )}
-      {playlistTrackData?.map((item, index) => (
-        <article key={index} className="mb-4 cursor-pointer">
-          {!isMobile && (
-            <div className="hidden md:grid md:grid-cols-[40px_80px_1fr_1fr_40px] md:items-center py-2">
-              <div className="flex justify-center items-center"></div>
-              <div className="flex items-center justify-center">
-                <img
-                  src={item?.imageList?.[0]?.url || catImg}
-                  alt={item.name}
-                  className="w-[60px] h-[60px] rounded-sm"
-                />
-              </div>
-              <div
-                className="flex items-center px-2 font-bold text-text-base text-sm hover:underline overflow-hidden whitespace-nowrap truncate max-w-[250px]"
-                title={item.name}
-                onClick={() =>
-                  openYoutubeSearch(item.name, item.trackArtists[0]?.name || "")
-                }
-              >
-                {item.name}
-              </div>
+      {!isLoading && (!playlistTrackData || playlistTrackData.length === 0) && (
+        <p className="text-sm text-center text-[#666] mt-10">
+          플레이리스트에 대한 트랙 정보가 없습니다.
+        </p>
+      )}
+      {!isLoading &&
+        Array.isArray(playlistTrackData) &&
+        playlistTrackData.map((item, index) => (
+          <article key={index} className="mb-4 cursor-pointer">
+            {!isMobile && (
+              <div className="hidden md:grid md:grid-cols-[40px_80px_1fr_1fr_40px] md:items-center py-2">
+                <div className="flex justify-center items-center"></div>
+                <div className="flex items-center justify-center">
+                  <img
+                    src={item?.imageList?.[0]?.url || catImg}
+                    alt={item.name}
+                    className="w-[60px] h-[60px] rounded-sm"
+                  />
+                </div>
+                <div
+                  className="flex items-center px-2 font-bold text-text-base text-sm hover:underline overflow-hidden whitespace-nowrap truncate max-w-[250px]"
+                  title={item.name}
+                  onClick={() =>
+                    openYoutubeSearch(
+                      item.name,
+                      item.trackArtists[0]?.name || ""
+                    )
+                  }
+                >
+                  {item.name}
+                </div>
 
-              <div
-                className="flex items-center text-gray-100 px-2 text-sm overflow-hidden whitespace-nowrap truncate max-w-[160px]"
-                title={item.trackArtists[0]?.name}
-              >
-                {item.trackArtists[0]?.name}
-              </div>
+                <div
+                  className="flex items-center text-gray-100 px-2 text-sm overflow-hidden whitespace-nowrap truncate max-w-[160px]"
+                  title={item.trackArtists[0]?.name}
+                >
+                  {item.trackArtists[0]?.name}
+                </div>
 
-              <div className="flex justify-center items-center">
-                <img src={sideMenu} alt="메뉴" className="cursor-pointer" />
-              </div>
-            </div>
-          )}
-
-          {isMobile && (
-            <div className="flex md:hidden justify-between items-center w-full mt-3">
-              <div
-                className="flex items-center gap-5"
-                onClick={() =>
-                  openYoutubeSearch(item.name, item.trackArtists[0]?.name || "")
-                }
-              >
-                <img
-                  src={item?.imageList?.[0]?.url || catImg}
-                  alt={item.name}
-                  className="w-[50px] h-[50px] rounded-sm"
-                />
-                <div className="flex flex-col gap-1 max-w-[160px]">
-                  <h2
-                    className="font-bold text-sm text-text-base overflow-hidden whitespace-nowrap truncate"
-                    title={item.name}
-                  >
-                    {item.name}
-                  </h2>
-
-                  {item.trackArtists[0]?.name && (
-                    <p
-                      className="text-xs text-gray-100 overflow-hidden whitespace-nowrap truncate"
-                      title={item.trackArtists[0].name}
-                    >
-                      {item.trackArtists[0].name}
-                    </p>
-                  )}
+                <div className="flex justify-center items-center">
+                  <img src={sideMenu} alt="메뉴" className="cursor-pointer" />
                 </div>
               </div>
-              <img src={sideMenu} alt="메뉴" className="cursor-pointer ml-2" />
-            </div>
-          )}
-        </article>
-      ))}
+            )}
+
+            {isMobile && (
+              <div className="flex md:hidden justify-between items-center w-full mt-3">
+                <div
+                  className="flex items-center gap-5"
+                  onClick={() =>
+                    openYoutubeSearch(
+                      item.name,
+                      item.trackArtists[0]?.name || ""
+                    )
+                  }
+                >
+                  <img
+                    src={item?.imageList?.[0]?.url || catImg}
+                    alt={item.name}
+                    className="w-[50px] h-[50px] rounded-sm"
+                  />
+                  <div className="flex flex-col gap-1 max-w-[160px]">
+                    <h2
+                      className="font-bold text-sm text-text-base overflow-hidden whitespace-nowrap truncate"
+                      title={item.name}
+                    >
+                      {item.name}
+                    </h2>
+
+                    {item.trackArtists[0]?.name && (
+                      <p
+                        className="text-xs text-gray-100 overflow-hidden whitespace-nowrap truncate"
+                        title={item.trackArtists[0].name}
+                      >
+                        {item.trackArtists[0].name}
+                      </p>
+                    )}
+                  </div>
+                </div>
+                <img
+                  src={sideMenu}
+                  alt="메뉴"
+                  className="cursor-pointer ml-2"
+                />
+              </div>
+            )}
+          </article>
+        ))}
     </section>
   );
 }
